6c1d1a06bb3d0168ecfb1f975462c920d6c1d825
EiEddie <eieddie4u@outlook.com>
2025-04-17 00:15:57 +0800

    feat: 接收器示例

---
+++ examples/receiver/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <uv.h>
+
+#include "serial.h"
+
+
+static uv_signal_t signal_handle;
+
+
+void msg_cb(struct msg_t msg) {
+	printf("\t{msg} a:%d [%x]; b:%d [%x]\n", msg.a, msg.a,
+	       msg.b, msg.b);
+}
+
+
+void on_signal(uv_signal_t* handle, int _) {
+	context_t* ctx = handle->data;
+
+	serial_recv_term(ctx);
+	serial_close(ctx);
+
+	uv_close((uv_handle_t*)handle, NULL);
+}
+
+
+int main(int argc, char* argv[]) {
+	const char* fname = argv[1];
+	context_t* ctx = serial_init();
+	serial_open(ctx, fname);
+	serial_recv_init(ctx, msg_cb);
+
+	uv_signal_init(ctx->loop, &signal_handle);
+	signal_handle.data = ctx;
+	uv_signal_start(&signal_handle, on_signal, SIGINT);
+
+	serial_run(ctx);
+
+	serial_free(ctx);
+	return 0;
+}
